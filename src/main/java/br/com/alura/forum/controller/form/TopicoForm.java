91a256c0d120bfd222e00eb48b606b18ea62c01a
Marcelo <dantasmr@yahoo.com.br>
2020-05-21 02:25:48 -0300

    swagger

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -5,40 +5,36 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
+import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import br.com.alura.forum.repository.TopicoRepository;
+import br.com.alura.forum.repository.CursoRepository;
 
-public class AtualizacaoTopicoForm {
-	
+public class TopicoForm {
+
 	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
 	
 	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
-
-	public String getTitulo() {
-		return titulo;
-	}
+	
+	@NotNull @NotEmpty
+	private String nomeCurso;
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
-	}
-
-	public String getMensagem() {
-		return mensagem;
 	}
 
 	public void setMensagem(String mensagem) {
 		this.mensagem = mensagem;
 	}
 
-	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
-		Topico topico = topicoRepository.getOne(id);
-		
-		topico.setTitulo(this.titulo);
-		topico.setMensagem(this.mensagem);
-		
-		return topico;
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
 	}
-	
+
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
+
 }
